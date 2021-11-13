382eb0c8f3bc2296bce43aca1d6ada5fdceb4be8
Fast2Service247 <fast2service247@gmail.com>
2021-11-13 16:29:44 +0530

    f2spro

--- fast2service.com/public_html/user_style.php
+++
@@ -1,9 +0,0 @@
-<style>
-    .bar-icon span{background: #cd2121;}
-    .navbar-brand.logo-small img{margin-left: -125px;}
-    .menu1{display: none!important;}
-    @media only screen and (max-width: 600px) {
-        .menu1{display: block!important;}
-        .menu{display: none!important;}
-     }
-</style>
